c40d2f31f0a44164847cd398adc4786a21011d7e
Ron Valstar <ron@ronvalstar.nl>
2017-06-27 21:27:31 +0200

    markdown to html task, fixes #88

--- README.md
+++ README.md
@@ -2,53 +2,47 @@
 
 This invoicing application stores all your data on your local machine.
  
-Project Invoice is accessible offline after the application has been loaded for the first time.
 Your local documents are not shared between different browsers or computers.
-Clearing your browser's data may **delete all your local documents!**. Backup your data regularly!
+Project Invoice is accessible offline after the application has been loaded for the first time. You can turn it into a mobile-web-app by using the add-to-home-screen from your mobile browser.
 
 
 ## Getting Started
 
-You can head to [Project Invoice](http://sjeiti.github.io/project-invoice) and start creating invoices.
+You can head to [Project Invoice](http://invoice.ronvalstar.nl) and start creating invoices.
 Or you can make a local installation.
+
+### Using the app
+
+The basis is easy: you have clients, clients have projects, projects have invoices (and an optional quotation).
+You can tweak what your invoice will look like. You can print it to pdf or png format.
+On the home or overview page you can check outstanding invoices, or mark them as paid.
+
+Everything you save is done so on the machine or device you are working on. To get your data from (say) your phone to your desktop you can export the data to json, and import that file on a different device.
+Clearing your browser's data may *delete all your local documents!*. Backup your data regularly!
 
 ### Running on localhost
 
-To get the application running you only need NodeJS, npm and GIT. If you don't know what those are you might be better off simply using the online version, your data will be just as safe.
+To get the application running locally you only need NodeJS, npm and GIT. If you don't know what those are you might be better off simply using the [online version](http://invoice.ronvalstar.nl), your data will be just as safe.
 
 `git clone https://github.com/Sjeiti/project-invoice.git`
+
 `npm i`
+
 `npm run start`
-
-
-## Why 
-
-Being self-employed all my life means I've been sending invoices for quite a while. I've seen a lot of invoicing software/services but was never really satisfied.
-So I created my own:
-
- - no cloud based data, everything is saved on your local machine, nothing is sent to a server
- - invoices are well designed and customizable
- - its easy to use
- - its open-source
- - its free
- - you can use the online version or run a local copy
-
-I cleaned it up a bit because I thought you might like it.
 
 
 ## Contributing
 
-The sources of this application are open-source and can be found at
-    [Github](https://github.com/Sjeiti/project-invoice).
+The sources of this application are open-source and can be found at [Github](https://github.com/Sjeiti/project-invoice).
     
-If you find a bug or have a good idea for a feature you can file it there.
+If you find a bug or have a good idea for a feature you can [file it there](https://github.com/Sjeiti/project-invoice/issues).
     
 
 ## faq
 
 ### Wait, this app is online in a browser. How is my private data not online?
 
-Every site can access something called LocalStorage which resides on your local machine. This is where your data is stored. Websites can never access the LocalStorage of another website. Never will data be sent from your computer to the server. The only network communication is the application logic being sent from the server to your computer. All your private data remains on your own computer.
+Every site can access something called LocalStorage which resides on your local machine or device. This is where your data is stored. Websites can never access the LocalStorage of another website. Never will data be sent from your computer to the server. The only network communication is the application logic being sent from the server to your computer. All your private data remains on your own computer.
 
 ### Is this safe?
 
@@ -66,7 +60,21 @@ Since the data is stored locally you can only access it from one computer. Howev
 
 There is a button for that on the settings page.
 
+### Why?
+
+Being self-employed all my life means I've been sending invoices for quite a while. I've seen a lot of invoicing software/services but was never really satisfied.
+So I created my own:
+
+ - no cloud based data, everything is saved on your local machine, nothing is sent to a server
+ - invoices are well designed and customizable
+ - its easy to use
+ - its open-source
+ - its free
+ - you can use the online version or run a local copy
+
+I cleaned it up a bit because I thought you might like it.
+
 
 ## License
 
-This project is licensed under the MIT License - see the LICENSE.md file for details
+This project is licensed under the MIT License - see the [LICENSE](https://raw.githubusercontent.com/Sjeiti/project-invoice/master/LICENSE) file for details
--- task/parseMarkdown.js
+++ task/parseMarkdown.js
@@ -6,12 +6,12 @@ const marked = require('marked')
         .usage('[options] <files ...>')
         .option('--source [source]', 'Source path')
         .option('--target [target]', 'Target path')
-        .option('--html [html]', 'HTML file')
-        .option('--injectGlob [injectGlob]', 'Globbing patterns for template injection')
-        .option('--bustCache [bustCache]', 'Bust cache by suffixing a random number')
         .parse(process.argv)
 
-console.log('commander',commander.source); // todo: remove log
+marked.setOptions({
+  breaks: true
+})
+
 utils.read(commander.source)
     .then(marked)
     .then(result=>{
@@ -20,223 +20,71 @@ utils.read(commander.source)
           ,{document} = window
           ,{body} = document
 
-      let element = document.querySelector('body>*') // body.firstChild.outerHTML
-      let fragment = document.createDocumentFragment()
-      // console.log('document',document.documentElement.firstChild); // todo: remove log
-      console.log('element',element); // todo: remove log
-      console.log('element.nextSibling',element.nextSibling); // todo: remove log
-      console.log('element.nextElementSibling',element.nextElementSibling); // todo: remove log
-      console.log('fragment',fragment); // todo: remove log
+      const sectionMark = ['H1','H2']
 
+      let element = document.querySelector('body>*')
+      const list = [[element]]
+      let [sectionArray] = list
 
+      while (element = element.nextSibling) {
+        const {nodeName} = element
+        if (sectionMark.includes(nodeName)) {
+          sectionArray = [element]
+          list.push(sectionArray)
+        } else {
+          sectionArray.push(element)
+        }
+      }
 
+      const fragment = document.createDocumentFragment()
+      list.forEach(sl=>{
+        const section = document.createElement('section')
+        sl.forEach(elm=>section.appendChild(elm))
+        fragment.appendChild(section)
+      })
 
+      body.innerHTML = ''
+      body.appendChild(fragment)
 
-                // jsdom.env(Object.assign(jsdomOptions || {}, {
-                //     html: contents.join(''),
-                //     done: (err, window) => {
-                //         global.window = window;
-                //         global.self = window.self;
-                //         global.document = window.document;
-                //         global.CustomEvent = window.CustomEvent;
-                //         global.Element = window.Element;
-                //         global.location = window.location;
-                //         global.history = window.history;
-                //         global.HTMLInputElement = window.HTMLInputElement;
-                //         global.HTMLTextAreaElement = window.HTMLTextAreaElement;
-                //         global.HTMLSelectElement = window.HTMLSelectElement;
-                //         ['href', 'search'].forEach(prop => {
-                //             Object.defineProperty(location, prop, {
-                //                 writable: true,
-                //                 value: location.href
-                //             });
-                //         });
-                //         init && init(err, window);
-                //         done();
-                //     }
-                // }))
-      // console.log('dom',dom); // todo: remove log
+      // anchor targets
+
+      Array.from(body.querySelectorAll('a')).forEach(a=>{
+        const href = a.getAttribute('href')
+            ,isExternal = /^http/.test(href)
+        isExternal&&a.setAttribute('target','_blank')
+      })
+
+      // faq
+
+      const heading = document.getElementById('faq')
+      element = heading
+      list.length = 0
+      while (element = element.nextSibling) {
+        const {nodeName} = element
+        if (nodeName==='H3') {
+          sectionArray = [element]
+          list.push(sectionArray)
+        } else {
+          sectionArray.push(element)
+        }
+      }
+      const parent = heading.parentNode
+      parent.innerHTML = ''
+      parent.appendChild(heading)
+      const afd = document.createElement('app-foldable-definition')
+      parent.appendChild(afd)
+      list.forEach(sl=>{
+        const dt = document.createElement('dt')
+        const dd = document.createElement('dd')
+        sl.forEach((elm,i)=>{
+          if (i===0) {
+            dt.innerHTML = elm.innerHTML
+          } else {
+            dd.appendChild(elm)
+          }
+        })
+        afd.appendChild(dt)
+        afd.appendChild(dd)
+      })
+      utils.save(commander.target,body.innerHTML)
     })
-    // .then(console.log.bind(console))
-
-// var uglify = require('uglify-js'),
-//     htmlMinify = require('html-minifier').minify,
-//     htmlparser = require('htmlparser2'),
-//     ngAnnotate,
-//     transpile,
-//     commander = require('commander')
-//         .usage('[options] <files ...>')
-//         .option('--source [source]', 'Source path')
-//         .option('--target [target]', 'Target path')
-//         .option('--html [html]', 'HTML file')
-//         .option('--injectGlob [injectGlob]', 'Globbing patterns for template injection')
-//         .option('--bustCache [bustCache]', 'Bust cache by suffixing a random number')
-//         .parse(process.argv),
-//     utils = require(__dirname+'/util/utils'),
-//     glomise = utils.glomise,
-//     read = utils.read,
-//     save = utils.save,
-//     extend = utils.extend,
-//     formatBytes = utils.formatBytes,
-//     //log = utils.log,
-//     warn = utils.warn,
-//     pass = o=>o,
-//     blockReplace = utils.blockReplace,
-//     logElapsed = utils.logElapsed,
-//     inlinePrefix = '/*inline23*/\n',
-//     inlinePrefixRegex = /^\/\*inline23\*\/\n/,
-//     /*warnxit = msg=>{
-//       console.warn(msg);
-//       process.exit(1);
-//     },*/
-//     uglifyOptions = {
-//       fromString: true,
-//       pure_getters: true
-//     },
-//     ngAnnotateOptions = {
-//       add: true,
-//       remove: false,
-//       singleQuotes: true,
-//       sourcemap: false // false||true||string
-//     },
-//     htmlMinifyOptions = {
-//       html5: true,
-//       removeComments: true,
-//       collapseBooleanAttributes: false,
-//       collapseWhitespace: true,
-//       collapseInlineTagWhitespace: false/*,
-//       minifyCSS:false,
-//       minifyJS:false*/
-//     },
-//     sourcePath = commander.source||'src/',
-//     targetPath = commander.target||'dist/',
-//     htmlPath = commander.html||'index.html',
-//     isBustCache = commander.bustCache||false,
-//     sourceHTML = sourcePath+htmlPath,
-//     targetHTML = targetPath+htmlPath,
-//     injectGlob = commander.injectGlob
-// ;
-//
-// console.log('Minifying',sourcePath+htmlPath,'to',targetPath);
-//
-// read(sourceHTML)
-//     .then(htmlSource=>{
-//       var buildComments = parseBuildComments(htmlSource);
-//       Promise.all(buildComments.map(buildComment=>
-//         // annotate and minify
-//         Promise.all(buildComment.scripts.map(file=>{
-//           return inlinePrefixRegex.test(file)?file:read(sourcePath+file);
-//         }))
-//             .then(sources=>sources.join('\n;//////////////////////////////////////////////////////////\n'))
-//             .then(source=>{
-//               var resolved = Promise.resolve(source);
-//               buildComment.processes.forEach(process=>{
-//                 if (process==='annotate')       resolved = resolved.then(source=>ngAnnotate(source,ngAnnotateOptions).src,warn);
-//                 else if (process==='inject')    resolved = resolved.then(inject,warn);
-//                 else if (process==='transpile') resolved = resolved.then(source=>transpile.transform(source,{presets: ['es2015']}).code,warn);
-//                 else if (process==='minify')    resolved = resolved.then(source=>uglify.minify(source,uglifyOptions).code,warn);
-//               });
-//               return resolved;
-//             })
-//             .then(source=>save(targetPath+buildComment.target,source))
-//             .then(pass,warn)
-//         ).concat((()=>{
-//           // replace html blocks
-//           var matchStart = /build((:\w+)+)\s+([^\s]*)/,
-//               matchEnd = /<!--\s?\/build\s?-->/,
-//               replacedSource = blockReplace(
-//                   htmlSource,
-//                   matchStart,
-//                   matchEnd,
-//                   line=>'<script src="'+line.match(matchStart).pop()+(isBustCache?'?nocache='+Math.random():'')+'"></script>'
-//               ),
-//               newSource = htmlMinify(replacedSource,extend({
-//                 maxLineLength:500
-//               },htmlMinifyOptions));
-//           return save(targetHTML,newSource).then(pass,warn);
-//         })())
-//       ).then(logElapsed,warn);
-//     },warn)
-// ;
-//
-// /**
-//  * Parses a HTML source to find build comments.
-//  * @param {String} htmlSource
-//  * @returns {Object[]}
-//  * @example
-//  *   <!-- build:minify:annotate /project-c/vendors.js -->...<!-- /build -->
-//  *   Returns {
-//  *    comment: '<!-- build:minify /project-c/vendors.js -->',
-//  *    target: '/project-c/vendors.js',
-//  *    processes: ['minify','annotate'],
-//  *    scripts: --an array with all script@src's found between the comments--
-//  *   }
-//  */
-// function parseBuildComments(htmlSource){
-//   var buildComments = []
-//       ,startBuild = /build((:\w+)+)\s+([^\s]*)/
-//       ,endBuild = /\/build/
-//       ,current
-//       ,hasContent = false
-//       ,parser = new htmlparser.Parser({
-//         oncomment: comment=>{
-//           var isEndBuild = endBuild.test(comment)
-//               ,isStartBuild = startBuild.test(comment)
-//               ,matchTarget = comment.match(startBuild)
-//               ,target = matchTarget&&matchTarget.pop()
-//               ,processes = matchTarget&&matchTarget[1].substr(1).split(':');
-//           //
-//           if (processes&&processes.indexOf('annotate')!==-1) ngAnnotate = require('ng-annotate');
-//           if (processes&&processes.indexOf('transpile')!==-1) transpile = require('babel-core');//require('es6-transpiler');
-//           //
-//           if (isEndBuild) {
-//             if (current) {
-//               buildComments.push(current);
-//               current = null;
-//             }
-//           } else if (isStartBuild) {
-//             current = {
-//               comment: comment,
-//               target: target,
-//               processes: processes,
-//               scripts: []
-//             };
-//           }
-//         }
-//         ,onopentag: (name,attribs)=>{
-//           var isScript = name==='script'
-//               ,src = attribs.src;
-//           if (isScript&&!src) hasContent = true;
-//           current&&current.scripts&&isScript&&src&&current.scripts.push(src);
-//         }
-//         ,onclosetag: name=>{
-//           if (name==='script') hasContent = false;
-//         }
-//         ,ontext: text=>{
-//           current&&current.scripts&&hasContent&&text&&current.scripts.push(inlinePrefix+text);
-//         }
-//       }, {decodeEntities: true});
-//   parser.write(htmlSource);
-//   parser.end();
-//   //
-//   return buildComments;
-// }
-//
-// /**
-//  * Injects directive and partial html into the run script (to prevent server calls)
-//  * @param {String} source
-//  */
-// function inject(source){
-//   var rxBasePath = new RegExp(sourcePath.replace(/([\.\/])/g,'\\$1'));
-//   return glomise(injectGlob)
-//     .then(paths=>Promise.all(paths.map(read)).then(results=>results.map(contents=>htmlMinify(contents,htmlMinifyOptions)).map((contents,i)=>'<script type="text/ng-template" id="'+paths[i].replace(rxBasePath,'')+'">'+contents+'</script>').join(''),warn),warn)
-//     .then(partials=>{
-//       console.log('injecting',formatBytes(partials.length),'of html');
-//       return blockReplace(
-//           source,
-//           /\/\*inject\*\//,
-//           /\/\*\/inject\*\//,
-//           '\''+partials.replace(/'/g,'\\\'').replace(/\r\n|\r|\n/g,'')+'\''
-//       );
-//     },warn);
-// }
