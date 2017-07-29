0d7eb67b4e865275f79cda699a03db30aee8868d
Ron Valstar <github@sjeiti.com>
2017-07-29 16:49:16 +0200

    added jsdoc to mixins, see #4

--- src/app/mixins.ts
+++ src/app/mixins.ts
@@ -3,15 +3,30 @@ import {IProject} from './interface/project'
 
 let datePipe:DatePipe = new DatePipe('en')
 
+/**
+ * Convert dateTime to date using ng DatePipe
+ * @param {string} dateTime
+ * @returns {string}
+ */
 export const dateTimeToDate = (dateTime = '')=> {
   return datePipe.transform(dateTime||(new Date).toString(), 'yyyy-MM-dd')
 }
 
+/**
+ * Test whether a property is an accessor (as in getter opposed to regular property)
+ * @param {object} obj
+ * @param {string} prop
+ * @returns {boolean}
+ */
 export const isPropertyAccessor = (obj:any, prop:string):boolean => {
   let desc = Object.getOwnPropertyDescriptor(obj, prop)
   return desc&&desc.hasOwnProperty&&(desc.hasOwnProperty('get')||desc.hasOwnProperty('set'))
 }
 
+/**
+ * Turns off enumerability of accessors in an object
+ * @param {object} obj
+ */
 export const dontEnumerateAccessors = (obj:any)=>{
     Object.keys(obj)
         .filter(prop=>isPropertyAccessor(this, prop))
