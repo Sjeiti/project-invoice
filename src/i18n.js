0e5258ea1a5f270b6ae814c6c3d209a177ed44a6
Ron Valstar <ron@ronvalstar.nl>
2018-05-04 19:07:23 +0200

    build task indexing i18n and lazy loading 2ndary, see #154

---
+++ src/i18n.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+
+Vue.use(VueI18n)
+const i18n = new VueI18n({
+    locale: 'nl'
+    ,messages: {
+        en: {
+            message: {
+                hello: 'hello world'
+            }
+            ,save: 'save'
+            ,revert: 'revert'
+            ,delete: 'delete'
+            ,language: 'language'
+            ,home: 'home'
+            ,overview: 'overview'
+            ,quarter: 'quarter'
+            ,clients: 'clients'
+            ,settings: 'settings'
+            ,layout: 'layout'
+            ,data: 'data'
+            ,copy: 'copy'
+            ,about: 'about'
+        }/*,nl: {
+            message: {
+                hello: 'hallo wereld'
+            }
+            ,save: 'bewaar'
+            ,revert: 'herstel'
+            ,delete: 'verwijder'
+            ,language: 'taal'
+            ,home: 'home'
+            ,overview: 'overzicht'
+            ,quarter: 'kwartaal'
+            ,clients: 'klanten'
+            ,settings: 'instellingen'
+            ,layout: 'opmaak'
+            ,data: 'data'
+            ,copy: 'tekst'
+            ,about: 'over'
+        }*/
+    }
+})
+
+fetch('/static/i18n/nl.json')
+    .then(res=>res.json())
+    .then(nl=>{
+        console.log('nl',nl) // todo: remove log
+        i18n.setLocaleMessage('nl',nl)
+        i18n.locale = 'en'
+        i18n.locale = 'nl'
+    })
+
+export default i18n
---
+++ task/i18n.js
@@ -0,0 +1,64 @@
+const utils = require(__dirname+'/util/utils')
+const save = utils.save
+const read = utils.read
+const glomise = utils.glomise
+const fileBase = './i18n/base.json'
+const globSrc = './src/**/*.+(vue|js)'
+const globFiles = './public/static/i18n/*.json'
+
+Promise.all([
+    readSrc(globSrc,fileBase)
+    ,readExisting(globFiles)
+])
+    .then(([keys,[paths,contents]])=>{
+    contents.forEach((content,i)=>{
+    const path = paths[i]
+    let key
+    let added = false
+    for (key in keys){
+    if (!content.hasOwnProperty(key)){
+        content[key] = key
+        added = true
+    }
+}
+added&&save(path,JSON.stringify(content,null,2))
+//
+const unused = []
+for (key in content){
+    !keys.hasOwnProperty(key)&&unused.push(key)
+}
+unused.length&&console.log(`unused keys in ${path.match(/\w+\.\w+$/).pop()}: ${unused.map(s=>`'${s}'`).join(', ')}`)
+})
+})
+
+/**
+ * Read source files for __('\w')
+ * @param {string} src
+ * @param {string} target
+ * @returns {Promise}
+ */
+function readSrc(src,target){
+    return glomise(src)
+            .then(paths=>Promise.all(paths.map(read)))
+.then(rslt=>rslt.join('').match(/__\('([^']+)'\)|v-__="'[^"']*|v-__?[^>]*>[^<]*/g)) // match js and directives
+// .then(rslt=>(console.log(rslt),rslt))
+.then(rslt=>rslt.map(fn=>fn.replace(/^__\('|'\)$|v-__="'|v-__?[^>]*>/g,''))) // clean matches
+.then(rslt=>rslt.filter(s=>s!==''&&s.substr(0,2)!=='{{')) // don't match interpolations
+// .then(rslt=>(console.log(rslt),rslt))
+.then(rslt=>(console.log(`found ${rslt.length} keys`),rslt))
+.then(rslt=>rslt.reduce((o,s)=>(o[s]=s,o),{}))
+.then(obj=>(save(target,JSON.stringify(obj)),obj))
+}
+
+/**
+ * Read existing localisation files
+ * @param {string} globFiles
+ * @returns {Promise}
+ */
+function readExisting(globFiles){
+    return glomise(globFiles)
+            .then(paths=>Promise.all([
+            Promise.resolve(paths)
+            ,Promise.all(paths.map(path=>read(path).then(JSON.parse)))
+]))
+}
