92924afc82ac80992574e3ac9423b4def83a180d
Ron Valstar <ron@ronvalstar.nl>
2018-05-27 10:19:32 +0200

    clients page tweaks, see #60

--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -8,22 +8,22 @@
       <table class="hoverable">
         <thead>
         <tr>
-          <td v-_>nr</td>
+          <td v-_ class="hide-low text-align-right">nr</td>
           <td v-_>name</td>
-          <td v-_>invoices</td>
+          <td v-_ class="text-align-center">invoices</td>
           <td v-_>recent</td>
-          <td v-_>last</td>
-          <td v-_>paid</td>
+          <td v-_ class="hide-low">last</td>
+          <td v-_ class="hide-low text-align-center">paid</td>
         </tr>
         </thead>
         <tbody>
           <tr v-for="client in sortedClients" :key="client.nr" v-on:click="onRowClick(client,$event)">
-            <td class="text-align-right">{{client.nr}}</td>
+            <td class="hide-low text-align-right">{{client.nr}}</td>
             <td><router-link v-bind:to="client.uri">{{client.name}}</router-link></td>
             <td class="text-align-center">{{client.projects.length}} </td>
             <td><date v-if="client.latestProject" class="small nowrap" :value="client.latestProject.dateLatest" /></td>
-            <td><router-link v-if="client.latestProject" class="small" :to="client.latestProject.uri">{{client.latestProject.invoiceNr}}</router-link></td>
-            <td class="text-align-center"><i v-if="client.latestProject" v-bind:class="hasOpenInvoices(client)?'icon-close':'icon-mark'"></i></td>
+            <td class="hide-low"><router-link v-if="client.latestProject" class="small" :to="client.latestProject.uri">{{client.latestProject.invoiceNr}}</router-link></td>
+            <td class="hide-low text-align-center"><i v-if="client.latestProject" v-bind:class="hasOpenInvoices(client)?'icon-close':'icon-mark'"></i></td>
           </tr>
         </tbody>
       </table>
@@ -78,4 +78,5 @@ export default {
   .icon-close { color: $colorRed; }
   .icon-mark { color: #CCC; }
   tr>*:first-child { padding-right: 8px; }
+  tbody td:last-child i { font-size: 10px; }
 </style>
