570af69d3720cb6caa338139a597bc58982ea8bb
Ron Valstar <ron@ronvalstar.nl>
2018-09-13 10:12:15 +0200

    Updated packages with vulnerability dependencies, fixes #169
    Fixed resulting issues

---
+++ cypress/plugins/index.js
@@ -0,0 +1,23 @@
+// cypress/plugins/index.js
+const webpack = require('@cypress/webpack-preprocessor')
+const webpackOptions = {
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  }
+}
+
+const options = {
+  // send in the options from your webpack.config.js, so it works the same
+  // as your app's code
+  webpackOptions,
+  watchOptions: {}
+}
+
+module.exports = on => {
+  on('file:preprocessor', webpack(options))
+}
