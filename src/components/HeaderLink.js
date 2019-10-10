5a83d1db80a8ff53a12c7c499316b38c07a87fac
Ron Valstar <ron@ronvalstar.nl>
2019-10-10 23:30:37 +0200

    refactored HeaderLink

--- src/components/HeaderLink.js
+++ src/components/HeaderLink.js
@@ -1,25 +1,18 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
+import styled, { css } from 'styled-components'
 
-const style = {
-  a: {
-    color: 'white'
-    , textDecoration: 'none'
-  }
-  , aCurrent: {
-    backgroundColor: '#333'
-    , color: 'white'
-  }
-}
+const StyledHeaderLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  ${props => props.current==='true' && css`
+    background-color: #333;
+    color: white;
+  `}
+`
 
-export const HeaderLink = withRouter(
-  ({ children, to, className, location: { pathname } }) => (
-    <Link
-      to={to}
-      className={className}
-      style={to === pathname ? style.aCurrent : style.a}
-    >
-      {children}
-    </Link>
-  )
-)
+export const HeaderLink = withRouter(attr => {
+  const { children, to, location: { pathname } } = attr
+  const isCurrent = to === pathname
+  return <StyledHeaderLink current={isCurrent.toString()} {...attr}>{children}</StyledHeaderLink>
+})
