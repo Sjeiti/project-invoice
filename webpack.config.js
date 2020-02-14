c86d5fea557b7c55f25564cb2dc360a4afad8ca7
Ron Valstar <ron@ronvalstar.nl>
2020-02-14 13:51:19 +0100

    copy public to dist in production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,59 @@
 const webpack = require('webpack')
 const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-module.exports = {
-  entry: './src/index.js'
-  ,output: {
-    path: path.resolve(__dirname, 'dist')
-    ,filename: 'bundle.js'
-  }
-  ,devServer: {
-    contentBase: './public'
-    ,port: 4212
-    ,historyApiFallback: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/
-        ,use: 'babel-loader'
-        ,exclude: /node_modules/
-      }
-      ,{
-        test: /\.scss$/
-        ,use: [
-          'style-loader'
-          ,'css-loader'
-          ,'sass-loader'
-        ]
-      }
-      ,{
-        test: /\.(html|css)/
-        ,use: ['raw-loader']
-      }
+module.exports = env => {
+  const isProduction = !!env&&env.production
+  const mode = isProduction?'production':'development'
+  console.log('mode',mode) // todo: remove log
+  return {
+    mode
+    ,entry: './src/index.js'
+    ,output: {
+      path: path.resolve(__dirname, 'dist')
+      ,filename: 'bundle.js'
+    }
+    ,devServer: {
+      contentBase: './public'
+      ,port: 4212
+      ,historyApiFallback: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/
+          ,use: 'babel-loader'
+          ,exclude: /node_modules/
+        }
+        ,{
+          test: /\.scss$/
+          ,use: [
+            'style-loader'
+            ,'css-loader'
+            ,'sass-loader'
+          ]
+        }
+        ,{
+          test: /\.(html|css)/
+          ,use: ['raw-loader']
+        }
+      ]
+    }
+    ,resolve: {
+      extensions: [
+        '.js'
+        ,'.jsx'
+        ,'.json'
+        ,'.html'
+      ]
+    }
+    ,plugins: [
+      new webpack.DefinePlugin({
+        'process.env.VERSION': JSON.stringify(require('./package.json').version)
+      })
+      ,new CopyWebpackPlugin(isProduction&&[
+          { from: 'public', to: './', toType: 'dir', dot: true}
+      ]||[], {})
     ]
   }
-  ,resolve: {
-    extensions: [
-      '.js'
-      ,'.jsx'
-      ,'.json'
-      ,'.html'
-    ]
-  }
-  ,plugins: [
-    new webpack.DefinePlugin({
-      'process.env.VERSION': JSON.stringify(require('./package.json').version)
-    })
-  ]
 }
