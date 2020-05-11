462a7c2aec5fade512b66e2b009e5e427e49bc2c
Ron Valstar <ron@ronvalstar.nl>
2020-05-11 16:50:29 +0200

    reverted original storage name

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ export const APP_NAME = 'Project Invoice'
 export const ORIGIN = 'https://projectinvoice.nl'
 export const VERSION = process.env.VERSION
 export const MODE_DEVELOPMENT = process.env.NODE_ENV==='development'
-export const STORAGE_NAME = 'pi'
+export const STORAGE_NAME = 'data'
 export const CLOUD_NAME = 'nl.projectinvoice.data.test'
 export const NOOP_ACTIONS = [STORE_SESSION]
 export const TODAY = new Date
